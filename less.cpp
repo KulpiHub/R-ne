9c99f424a9e516704aa0fe8f3f24cb16e203c82e
KulpiHub <83190881+KulpiHub@users.noreply.github.com>
2021-05-15 21:28:19 +0200

    Update less.cpp

--- less.cpp
+++ less.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
 double less(double a, double b)
+double less(double a, double b)
 {
     return (a < b ? a : b);
 }
 int main()
 {
 
-    double a, b;
+    double a;
+    double b;
     std::cin>>a;
     std::cin>>b;
     std::cout << less(a, b) << std::endl;
