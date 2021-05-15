9a4c85b25c8b4d7c64124808be51b5eef2071898
KulpiHub <83190881+KulpiHub@users.noreply.github.com>
2021-05-15 21:28:53 +0200

    Update Fraction.cpp

--- Fraction.cpp
+++ Fraction.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cmath>
-
+void fraction(double &integral, double &fractional, double param);
 void fraction(double &integral, double &fractional, double param) {
 	fractional = modf(param, &integral);
 }
 
 int main() {
-	double integral, fractional;
+	double integral;
+	double fractional;
 	fraction(integral, fractional, 3.14159);
 	std::cout << integral << " " << fractional << std::endl;
 }
