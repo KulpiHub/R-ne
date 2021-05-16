7972ec44cdac689a55430554bd6301a5be248192
KulpiHub <83190881+KulpiHub@users.noreply.github.com>
2021-05-17 00:49:34 +0200

    Update enumerate.cpp

--- enumerate.cpp
+++ enumerate.cpp
@@ -1,15 +1,15 @@
 #include <fstream>
 
 int main(){
-    int count = 1;
+    int counter = 1;
     std::string line;
     std::ifstream input("input.txt");
     std::ofstream output("output.txt");
         while(!input.eof())
     {
             getline(input, line);
-            output << count << " " << line << "\n";
-            count++;
+            output << counter << " " << line << "\n";
+            counter++;
     }
     output.close();
     input.close();
